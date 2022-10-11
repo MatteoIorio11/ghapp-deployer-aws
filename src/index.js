e50ff898d90a1e2fbd658062b62cde1d55d27b42
Matteo <matteo.iorio01@gmail.com>
2022-10-11 21:48:47 +0200

    modified index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 /**
  * @param {import('probot').Probot} app
  */
+ module.exports = (app) => {
+  app.log("Yay! The app was loaded!");
 
-module.exports = (app) => {
-    app.on("issues.opened", async (context) => {
-      // `context` extracts information from the event, which can be passed to
-      // GitHub API calls. This will return:
-      //   { owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World !}
-      const params = context.issue({ body: "Hello World!" });
-  
-      // Post a comment on the issue
-      return context.octokit.issues.createComment(params);
-    });
-  };
+  app.on("issues.opened", async (context) => {
+    return context.octokit.issues.createComment(
+      context.issue({ body: "Hello, World!" })
+    );
+  });
+};
