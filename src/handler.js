48068d96e27848050fab21204670bea79466800c
Matteo <matteo.iorio01@gmail.com>
2022-10-11 21:49:20 +0200

    handler js

--- src/handler.js
+++ src/handler.js
@@ -1,10 +1,10 @@
-// handler.js
 const {
-    createLambdaFunction,
-    createProbot,
-  } = require("@probot/adapter-aws-lambda-serverless");
-  const appFn = require("../app");
-  
-  module.exports.webhooks = createLambdaFunction(appFn, {
-    probot: createProbot(),
-  });
+  createLambdaFunction,
+  createProbot,
+} = require("@probot/adapter-aws-lambda-serverless");
+
+const appFn = require("./");
+
+module.exports.webhooks = createLambdaFunction(appFn, {
+  probot: createProbot(),
+});
