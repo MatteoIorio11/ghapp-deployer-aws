d7713a5201cb4b9b77c96c8c0eabf64e8e56aed2
Matteo <matteo.iorio01@gmail.com>
2022-10-12 10:21:51 +0200

    update the app.js and the handler.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
  */
  module.exports = (app) => {
   app.on("issues.opened", async (context) => {
+    console.log('Received event:', JSON.stringify(event, null, 2));
     return context.octokit.issues.createComment(
       context.issue({ body: "Hello, World!" })
     );
