a2f492411fd7beaf362d584de5809c79f1b77eec
Matteo <matteo.iorio01@gmail.com>
2022-10-12 09:33:39 +0200

    modified handler.js

--- src/handler.js
+++ src/handler.js
@@ -5,6 +5,6 @@ const {
 } = require("@probot/adapter-aws-lambda-serverless");
 const appFn = require("./app");
 
-exports.webhooks = createLambdaFunction(appFn, {
+module.exports.webhooks = createLambdaFunction(appFn, {
   probot: createProbot(),
 });
