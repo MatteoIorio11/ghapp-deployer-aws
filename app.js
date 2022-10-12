e24cce5e9ff1f4d1d2ed7741583b6724eafa25f5
Matteo <matteo.iorio01@gmail.com>
2022-10-12 10:09:51 +0200

    changed the app.js

--- app.js
+++ app.js
@@ -2,8 +2,6 @@
  * @param {import('probot').Probot} app
  */
  module.exports = (app) => {
-  app.log("Yay! The app was loaded!");
-
   app.on("issues.opened", async (context) => {
     return context.octokit.issues.createComment(
       context.issue({ body: "Hello, World!" })
