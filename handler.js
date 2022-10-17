7d98e029c0160994e47cf7e4dae41d14116cd7d9
Matteo <70414065+MatteoIorio11@users.noreply.github.com>
2022-10-17 15:52:18 +0200

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,10 +1,12 @@
 // handler.js
-const {
-  createLambdaFunction,
-  createProbot,
-} = require("@probot/adapter-aws-lambda-serverless");
+const { Probot } = require('probot');
 const appFn = require("./app");
 
-module.exports.webhooks = createLambdaFunction(appFn, {
-  probot: createProbot(),
-});
+module.exports.webhooks = async (event) => {
+  let probot = new Probot({
+        appId: process.env.APP_ID,
+        privateKey: Buffer.from(process.env.PRIVATE_KEY, 'base64').toString('utf-8'),
+        secret: process.env.WEBHOOK_SECRET,
+        logLevel: process.env.PROBOT_LOG_LEVEL || 'info'
+    });
+};
