0442cd8ab0a8192eaf21db7ff488ad7a823cc550
Matteo <70414065+MatteoIorio11@users.noreply.github.com>
2022-10-17 16:18:30 +0200

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,22 +1,10 @@
-const { Probot } = require('probot');
-const autoMeBot = require('./auto-me-bot');
+// handler.js
+const {
+  createLambdaFunction,
+  createProbot,
+} = require("@probot/adapter-aws-lambda-serverless");
+const appFn = require("./app");
 
-
-// handler function for aws lambda >> 'Handler:src/app-runner.handler'
-module.exports.webhooks = async (event) => {
-    let probot = new Probot({
-        appId: process.env.APP_ID,
-        privateKey: Buffer.from(process.env.PRIVATE_KEY, 'base64').toString('utf-8'),
-        secret: process.env.WEBHOOK_SECRET,
-        logLevel: process.env.PROBOT_LOG_LEVEL || 'info'
-    });
-
-    await probot.load(autoMeBot);
-
-    return probot.webhooks.verifyAndReceive({
-        id: event.headers['x-github-delivery'],
-        name: event.headers['x-github-event'],
-        signature: event.headers['x-hub-signature'],
-        payload: JSON.parse(event.body)
-    });
-};
+module.exports.webhooks = createLambdaFunction(appFn, {
+  probot: createProbot(),
+});
