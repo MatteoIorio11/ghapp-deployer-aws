2bcad61c0ed3beeba12b751afe924e775a02d317
Matteo <matteo.iorio01@gmail.com>
2022-10-27 21:55:05 +0200

    test the app

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 /**
  * @param {import('probot').Probot} app
  */
+ const jobFilter = process.env.JOB_FILTER;
  module.exports = (app) => {
   app.log("Yay! The app was loaded!");
 
   app.on("issues.opened", async (context) => {
     return context.octokit.issues.createComment(
-      context.issue({ body: "Hello, World!" })
+      context.issue({ body: "Hello, World!" + jobFilter })
     );
   });
 };
