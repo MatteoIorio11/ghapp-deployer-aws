45bfeb45c928d0d14a4b7e396f8fccca099a6aa3
Matteo <matteo.iorio01@gmail.com>
2022-11-05 20:37:24 +0100

    added the decrypt.js module, where I decrypt all the env variables and pass them to the handler in order to create the probot. EVERYTHING WORK

--- handler.js
+++ handler.js
@@ -1,10 +1,26 @@
+
 // handler.js
 const {
-  createLambdaFunction,
-  createProbot,
-} = require("@probot/adapter-aws-lambda-serverless");
+    createLambdaFunction,
+    createProbot,
+    Probot,
+  } = require("@probot/adapter-aws-lambda-serverless");
 const appFn = require("./app");
+const spawn = require('child_process').execSync
 
-module.exports.webhooks = createLambdaFunction(appFn, {
-  probot: createProbot(),
-});
+function decryptVariables(){
+  const child = spawn('node decrypt.js');
+  var string = child.toString('ascii');
+  var env_v = JSON.parse(string);
+  process.env['APP_ID']=env_v.APP_ID;
+  process.env['PRIVATE_KEY']=env_v.PRIVATE_KEY;
+  process.env['WEBHOOK_SECRET']=env_v.WEBHOOK_SECRET;
+  var probot = createProbot();
+  return probot;
+}
+
+module.exports.webhooks = createLambdaFunction(appFn, { 
+      probot : decryptVariables(),
+  });
+
+
