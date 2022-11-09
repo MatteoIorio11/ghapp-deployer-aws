6289f09d890383dd074f92d5699bc84887b307c2
Matteo <matteo.iorio01@gmail.com>
2022-11-09 12:48:38 +0100

    encryption of the env variables inside the handler

--- handler.js
+++ handler.js
@@ -7,22 +7,60 @@ const {
   } = require("@probot/adapter-aws-lambda-serverless");
 const appFn = require("./app");
 const spawn = require('child_process').execSync
+const fun = require('./function');
+const AWS = require('aws-sdk');
+AWS.config.update({ region: 'us-east-1' });
+
+const functionName = process.env.AWS_LAMBDA_FUNCTION_NAME;
+
+const encrypted_app_id = process.env['APP_ID'];
+const encrypted_webhook_secret = process.env['WEBHOOK_SECRET'];
+const encrypted_private_key = process.env['PRIVATE_KEY'];
 
 
-function decryptVariables(){
-  const child = spawn('node decrypt.js');
-  var string = child.toString('ascii');
-  var env_v = JSON.parse(string);
-  process.env['APP_ID']=env_v.APP_ID;
-  process.env['PRIVATE_KEY']="\"".concat(env_v.PRIVATE_KEY).concat("\"");
-  process.env['WEBHOOK_SECRET']=env_v.WEBHOOK_SECRET;
-  
+let decrypted= {};
+
+function processEvent(event){
   var probot = createProbot();
-  return probot;
+  probot.load(appFn);
+  return fun(probot, event);
 }
 
-module.exports.webhooks = createLambdaFunction(appFn, { 
-      probot : decryptVariables(),
-  });
+
+module.exports.webhooks = async (event) => {
+  if ( decrypted.APP_ID && decrypted.PRIVATE_KEY && decrypted.WEBHOOK_SECRET ) {
+
+  } else {
+      const kms = new AWS.KMS();
+      const req1 = {
+          CiphertextBlob: Buffer.from(encrypted_app_id, 'base64'),
+          EncryptionContext: { LambdaFunctionName: functionName },
+      };
+      const req2 = {
+          CiphertextBlob: Buffer.from(encrypted_private_key, 'base64'),
+          EncryptionContext: { LambdaFunctionName: functionName },
+      };
+      const req3 = {
+          CiphertextBlob: Buffer.from(encrypted_webhook_secret, 'base64'),
+          EncryptionContext: { LambdaFunctionName: functionName },
+      };
+      try {
+          var data_a = await kms.decrypt(req1).promise();
+          var data_p = await kms.decrypt(req2).promise();
+          var data_w = await kms.decrypt(req3).promise();
+          decrypted.APP_ID= data_a.Plaintext.toString('ascii');
+          decrypted.PRIVATE_KEY = data_p.Plaintext.toString('ascii');
+          decrypted.WEBHOOK_SECRET = data_w.Plaintext.toString('ascii');
+      } catch (err) {
+          console.log('Decrypt error:', err);
+          throw err;
+      }
+  }
+  process.env['APP_ID'] = decrypted.APP_ID;
+  process.env['PRIVATE_KEY'] = "\"".concat(decrypted.PRIVATE_KEY).concat("\"");
+  process.env['WEBHOOK_SECRET'] = decrypted.WEBHOOK_SECRET;
+  console.log(decrypted.PRIVATE_KEY);
+  return processEvent(event);
+}
 
 
