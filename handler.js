d2027aa59c5890f357564e55f9c816086dddba38
Matteo <matteo.iorio01@gmail.com>
2022-11-22 21:59:22 +0100

    added comments

--- handler.js
+++ handler.js
@@ -18,8 +18,12 @@ function processEvent(event){
 //Handler of the function 
 module.exports.webhooks = async (event) => {
     let client = new AWS.SecretsManager();
+    //Get the secrets and parse them as JSON 
     let data = JSON.parse((await client.getSecretValue({SecretId: 'dev/mlh-test'}).promise()).SecretString);
+    //Assign the value of the secrets to the env in order to create the Probot app
     process.env['APP_ID'] = data["APP_ID"];
+    // TIP : the PRIVATE_KEY must be : ("---BEGIN-RSA--- ....") and not (---BEGIN-RSA--- ....) the ' " " ' are a MUST. So if you
+    // add the Secret as "---BEGIN...." you have to use ; process.env['PRIVATE_KEY'] = data["PRIVATE_KEY"]; 
     process.env['PRIVATE_KEY'] = "\"".concat(data["PRIVATE_KEY"]).concat("\"");
     process.env['WEBHOOK_SECRET'] = data["WEBHOOK_SECRET"];
     return processEvent(event);
