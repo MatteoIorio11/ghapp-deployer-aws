c4a8ab688e463397f25e39197b2bfbb690465f56
Matteo <matteo.iorio01@gmail.com>
2022-11-24 22:18:52 +0100

    fix comments

--- handler.js
+++ handler.js
@@ -4,6 +4,7 @@ const {
     createProbot,
   } = require("@probot/adapter-aws-lambda-serverless");
 const appFn = require("./app");
+const testFn = require("./test-function");
 const lambda_function = require('./function');
 const AWS = require('aws-sdk');
 AWS.config.update({ region: 'us-east-1' });
@@ -11,7 +12,7 @@ AWS.config.update({ region: 'us-east-1' });
 //Create the probot and put inside it the app. The app is the "filter" on what we have to do when the function is triggered
 function processEvent(event){
     let probot = createProbot();
-    probot.load(appFn);
+    probot.load(testFn);
     return lambda_function(probot, event);
 } 
 
@@ -22,8 +23,7 @@ module.exports.webhooks = async (event) => {
     let data = JSON.parse((await client.getSecretValue({SecretId: 'dev/mlh-test'}).promise()).SecretString);
     //Assign the value of the secrets to the env in order to create the Probot app
     process.env['APP_ID'] = data["APP_ID"];
-    // TIP : the PRIVATE_KEY must be : ("---BEGIN-RSA--- ....") and not (---BEGIN-RSA--- ....) the ' " " ' are a MUST. So if you
-    // add the Secret as "---BEGIN...." 
+    // TIP : the PRIVATE_KEY must be : ("---BEGIN-RSA--- ....") and not (---BEGIN-RSA--- ....) the ' " " ' are a MUST. 
     let prv_key = data["PRIVATE_KEY"];
     if(prv_key.indexOf('"') < 0){
         process.env['PRIVATE_KEY'] = "\"".concat(prv_key).concat("\"");
